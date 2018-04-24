b63027c2109395763e92e81a13e58e38c22e6dde
Madde Nyblom <maddenyblom@hotmail.com>
2018-04-24 10:44:06 +0200

    Cool lighting

--- Direct3D_CPP_Flipped/PixelShader.hlsl
+++ Direct3D_CPP_Flipped/PixelShader.hlsl
@@ -13,14 +13,25 @@ struct VS_OUT
 
 float4 PS_main(VS_OUT input) : SV_TARGET
 {
-	float4 lightSource = input.cameraPos; //same as our camera since that is where we want our light to come from
-	float4 lightDir = lightSource - input.pointOnSurface;
-	lightDir = normalize(lightDir);
-	float diffConstant = dot(lightDir.xyz, input.Normal.xyz);
+	input.cameraPos.z = -4.0f;
+input.cameraPos.y -= 2.0f;
 
-	float4 color = texture1.Sample(textureSample, input.TexCoord);
-	float3 colorShaded = color * diffConstant + (0.2 * input.Color);
-	//float3 colorShaded = input.Color * diffConstant;
+float4 ambient = float4(0.0f, 0.0f, 0.1f, 1.0f);
+float4 lightColor = float4(1.0f, 0.7f, 0.4f, 1.0f);
+float4 lightSource = input.cameraPos; //same as our camera since that is where we want our light to come from
+float4 lightDir = lightSource - input.pointOnSurface;
+float lightDistance = length(lightDir);
+lightDir = normalize(lightDir);
+float diffConstant = dot(lightDir.xyz, input.Normal.xyz);
+float4 color = texture1.Sample(textureSample, input.TexCoord);
+float4 colorShaded;
 
-	return float4(colorShaded, color.w);
+
+colorShaded = color * lightColor * diffConstant;
+colorShaded += ambient;
+
+//color += colorShaded;
+//float3 colorShaded = input.Color * diffConstant;
+
+return float4(colorShaded.xyz, color.w);
 }
