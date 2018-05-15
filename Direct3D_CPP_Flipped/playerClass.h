85aa9985fef358b1635878fa66135d5cfcc6ba2e
Emil L <eoelindholm@gmail.com>
2018-05-15 14:10:08 +0200

    Fireball has collision and cooldown

--- Direct3D_CPP_Flipped/playerClass.h
+++ Direct3D_CPP_Flipped/playerClass.h
@@ -72,7 +72,6 @@ public:
 	void setRingType(int other);
 	int getRingType();
 
-
 	void setFireballCast(bool check);
 	bool getFireballCast();
 
@@ -174,6 +173,8 @@ private:
 	bool attackReleased;
 
 	bool fireballCast; //has player cast fireball?
+	bool fireballWasCast;
+	float fireballCooldown;
 
 	bool hasRing;
 	int ringType; // 0 = DOUBLEJUMP, 1 = FIREBALL, 2 = ???, 3 = ???, 4 = ???, 5 = ???, 6 = ???, 7 = ???, 8 = ???.
